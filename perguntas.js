67915144cb296799bead3841e67211368c156a7a
Matheus Henrique Pedroso Viana <matheus_pviana@hotmail.com>
2021-11-16 18:33:00 -0300

    Import
    Alterações principais

---
+++ perguntas.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, perguntas, options, and answers
+let perguntas = [
+    {
+    numb: 1,
+    pergunta: "A Naiane faz aniversário que dia?",
+    answer: "3 de março",
+    options: [
+      "3 de março",
+      "13 de março",
+      "22 de março",
+      "28 de março"
+    ]
+  },
+    {
+    numb: 2,
+    pergunta: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    pergunta: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    pergunta: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish pergunta Language",
+      "Stylesheet Query Language",
+      "Statement pergunta Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    pergunta: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add pergunta
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   pergunta: "Your pergunta is Here",
+  //   answer: "Correct answer of the pergunta is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
